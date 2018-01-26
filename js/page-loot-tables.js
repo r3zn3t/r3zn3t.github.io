bec9ec69d208f3c08b8cd3bc9fc4b8c58528c161
r3zn3t <34678544+r3zn3t@users.noreply.github.com>
2018-01-26 15:23:53 -0700

    Add files via upload

---
+++ js/page-loot-tables.js
@@ -0,0 +1,210 @@
+var gems = {
+	"price": {
+		"tenGems": {
+			"name": "<tr><th id='title-header' colspan='2'>10gp Gems</th></tr>", 
+			"die": "<tr><td id='title-data'><b>d12</b></td>",
+			"description": "<td id='title-data'><b>Stone Description</b></td</tr>",
+			"data": [
+				"<tr>",
+					"<td>1</td>",
+					"<td>Azurite (opaque mottled deep blue)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>2</td>",
+					"<td>Banded agate (translucent striped brown, blue, white, or red)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>3</td>",
+					"<td>Blue quartz (transparent pale blue)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>4</td>",
+					"<td>Eye agate (translucent circles of gray, white, brown, blue, or green)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>5</td>",
+					"<td>Hematite (opaque gray-black)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>6</td>",
+					"<td>Lapis lazuli (opaque light and dark blue with yellow flecks)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>7</td>",
+					"<td>Malachite (opaque striated light and dark green)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>8</td>",
+					"<td>Moss agate (translucent pink or yellow-white with mossy gray or green markings)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>9</td>",
+					"<td>Obsidian (opaque black)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>10</td>",
+					"<td>Rhodochrosite (opaque light pink)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>11</td>",
+					"<td>Tiger eye (translucent brown with golden center)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>12</td>",
+					"<td>Turquoise (opaque light blue-green)</td>",
+				"</tr>"
+			],
+		},
+		"fiftyGems": {
+			"name": "<tr><th id='title-header' colspan='2'>50gp Gems</th></tr>",
+			"die": "<tr><td id='title-data'><b>d12</b></td>",
+			"description": "<td id='title-data'><b>Stone Description</b></td</tr>",
+			"data": [
+				"<tr>",
+					"<td>1</td>",
+					"<td>Azurite (opaque mottled deep blue)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>2</td>",
+					"<td>Banded agate (translucent striped brown, blue, white, or red)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>3</td>",
+					"<td>Blue quartz (transparent pale blue)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>4</td>",
+					"<td>Eye agate (translucent circles of gray, white, brown, blue, or green)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>5</td>",
+					"<td>Hematite (opaque gray-black)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>6</td>",
+					"<td>Lapis lazuli (opaque light and dark blue with yellow flecks)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>7</td>",
+					"<td>Malachite (opaque striated light and dark green)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>8</td>",
+					"<td>Moss agate (translucent pink or yellow-white with mossy gray or green markings)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>9</td>",
+					"<td>Obsidian (opaque black)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>10</td>",
+					"<td>Rhodochrosite (opaque light pink)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>11</td>",
+					"<td>Tiger eye (translucent brown with golden center)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>12</td>",
+					"<td>Turquoise (opaque light blue-green)</td>",
+				"</tr>" 
+			],
+		},
+		"fiftyGems": {
+			"name": "<tr><th id='title-header' colspan='2'>50gp Gems</th></tr>",
+			"die": "<tr><td id='title-data'><b>d12</b></td>",
+			"description": "<td id='title-data'><b>Stone Description</b></td</tr>",
+			"data": [
+				"<tr>",
+					"<td>1</td>",
+					"<td>Azurite (opaque mottled deep blue)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>2</td>",
+					"<td>Banded agate (translucent striped brown, blue, white, or red)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>3</td>",
+					"<td>Blue quartz (transparent pale blue)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>4</td>",
+					"<td>Eye agate (translucent circles of gray, white, brown, blue, or green)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>5</td>",
+					"<td>Hematite (opaque gray-black)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>6</td>",
+					"<td>Lapis lazuli (opaque light and dark blue with yellow flecks)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>7</td>",
+					"<td>Malachite (opaque striated light and dark green)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>8</td>",
+					"<td>Moss agate (translucent pink or yellow-white with mossy gray or green markings)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>9</td>",
+					"<td>Obsidian (opaque black)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>10</td>",
+					"<td>Rhodochrosite (opaque light pink)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>11</td>",
+					"<td>Tiger eye (translucent brown with golden center)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>12</td>",
+					"<td>Turquoise (opaque light blue-green)</td>",
+				"</tr>" 
+			],
+		},
+		
+	},
+}
+
+
+//**********************************
+//* beginning of display scripting *
+//**********************************
+function tableChoice(tname){
+	var name = tname.name;
+	var die = tname.die;
+	var description = tname.description;
+	var data = tname.data;
+	var str = name+die+description+data;
+	return str;
+}
+
+var tengems = tableChoice(gems.price.tenGems);
+var fiftygems = tableChoice(gems.price.fiftyGems);
+
+//logic for dropdown filter selection
+function changeTable(){
+	if($("select#tablefilter").val() == 'All'){
+		$("h4#main-header").html("All");
+		$("table.loot-table").html("");
+		$("table#mytable1").html(tengems);
+	}
+	if($("select#tablefilter").val() == 'Gemstones'){
+		$("h4#main-header").html("Gemstones");
+		$("table#mytable1").html(tengems);
+		$("table#mytable2").html(fiftygems);
+	}
+}
+
+//onload of page
+function loadTable(){
+	$("h4#main-header").html("All");
+	$("table#mytable1").html(tengems);
+	//$("table#mytable2").html(fiftygems);
+}
+
+//main page initialization
+window.onload = loadTable;
