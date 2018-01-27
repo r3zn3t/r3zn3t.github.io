0cb09609660a8d3054043e81ca00c6afab70f951
r3zn3t <34678544+r3zn3t@users.noreply.github.com>
2018-01-26 17:20:49 -0700

    Update page-loot-tables.js

--- js/page-loot-tables.js
+++ js/page-loot-tables.js
@@ -1,7 +1,7 @@
 var gems = {
 	"price": {
 		"tenGems": {
-			"name": "<tr><th id='title-header' colspan='2'>10gp Gems</th></tr>", 
+			"name": "<tr><th id='title-header' colspan='2'>10gp Gemstones</th></tr>", 
 			"die": "<tr><td id='title-data'><b>d12</b></td>",
 			"description": "<td id='title-data'><b>Stone Description</b></td</tr>",
 			"data": [
@@ -56,116 +56,433 @@ var gems = {
 			],
 		},
 		"fiftyGems": {
-			"name": "<tr><th id='title-header' colspan='2'>50gp Gems</th></tr>",
+			"name": "<tr><th id='title-header' colspan='2'>50gp Gemstones</th></tr>",
 			"die": "<tr><td id='title-data'><b>d12</b></td>",
 			"description": "<td id='title-data'><b>Stone Description</b></td</tr>",
 			"data": [
 				"<tr>",
 					"<td>1</td>",
-					"<td>Azurite (opaque mottled deep blue)</td>",
+					"<td>Bloodstone (opaque dark gray with red flecks)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>2</td>",
-					"<td>Banded agate (translucent striped brown, blue, white, or red)</td>",
+					"<td>Carnelian (opaque orange to red-brown)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>3</td>",
-					"<td>Blue quartz (transparent pale blue)</td>",
+					"<td>Chalcedony (opaque white)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>4</td>",
-					"<td>Eye agate (translucent circles of gray, white, brown, blue, or green)</td>",
+					"<td>Chrysoprase (translucent green)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>5</td>",
-					"<td>Hematite (opaque gray-black)</td>",
+					"<td>Citrine (transparent pale yellow-brown)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>6</td>",
-					"<td>Lapis lazuli (opaque light and dark blue with yellow flecks)</td>",
+					"<td>Jasper (opaque blue, black, or brown)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>7</td>",
-					"<td>Malachite (opaque striated light and dark green)</td>",
+					"<td>Moonstone (translucent white with pale blue glow)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>8</td>",
-					"<td>Moss agate (translucent pink or yellow-white with mossy gray or green markings)</td>",
+					"<td>Onyx (opaque bands of black and white, or pure black or white)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>9</td>",
-					"<td>Obsidian (opaque black)</td>",
+					"<td>Quartz (transparent white, smoky gray, or yellow)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>10</td>",
-					"<td>Rhodochrosite (opaque light pink)</td>",
+					"<td>Sardonyx (opaque bands of red and white)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>11</td>",
-					"<td>Tiger eye (translucent brown with golden center)</td>",
+					"<td>Star rose quartz (translucent rosy stone with white star-shaped center)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>12</td>",
-					"<td>Turquoise (opaque light blue-green)</td>",
+					"<td>Zircon (transparent pale blue-green)</td>",
 				"</tr>" 
 			],
 		},
-		"fiftyGems": {
-			"name": "<tr><th id='title-header' colspan='2'>50gp Gems</th></tr>",
-			"die": "<tr><td id='title-data'><b>d12</b></td>",
+		"hundredGems": {
+			"name": "<tr><th id='title-header' colspan='2'>100gp Gemstones</th></tr>",
+			"die": "<tr><td id='title-data'><b>d10</b></td>",
 			"description": "<td id='title-data'><b>Stone Description</b></td</tr>",
 			"data": [
 				"<tr>",
 					"<td>1</td>",
-					"<td>Azurite (opaque mottled deep blue)</td>",
+					"<td>Amber (transparent watery gold to rich gold)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>2</td>",
-					"<td>Banded agate (translucent striped brown, blue, white, or red)</td>",
+					"<td>Amethyst (transparent deep purple)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>3</td>",
-					"<td>Blue quartz (transparent pale blue)</td>",
+					"<td>Chrysoberyl (transparent yellow-green to pale green)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>4</td>",
-					"<td>Eye agate (translucent circles of gray, white, brown, blue, or green)</td>",
+					"<td>Coral (opaque crimson)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>5</td>",
-					"<td>Hematite (opaque gray-black)</td>",
+					"<td>Garnet (transparent red, brown-green, or violet)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>6</td>",
-					"<td>Lapis lazuli (opaque light and dark blue with yellow flecks)</td>",
+					"<td>Jade (translucent light green, deep green , or white)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>7</td>",
-					"<td>Malachite (opaque striated light and dark green)</td>",
+					"<td>Jet (opaque deep black)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>8</td>",
-					"<td>Moss agate (translucent pink or yellow-white with mossy gray or green markings)</td>",
+					"<td>Pearl (opaque lustrous white , yellow, or pink)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>9</td>",
-					"<td>Obsidian (opaque black)</td>",
+					"<td>Spinel (transparent red, red-brown, or deep green)</td>",
 				"</tr>",
 				"<tr>",
 					"<td>10</td>",
-					"<td>Rhodochrosite (opaque light pink)</td>",
+					"<td>Tourmaline (transparent pale green , blue, brown , or red)</td>",
+				"</tr>",
+			],
+		},
+		"fivehundredGems": {
+			"name": "<tr><th id='title-header' colspan='2'>500gp Gemstones</th></tr>",
+			"die": "<tr><td id='title-data'><b>d6</b></td>",
+			"description": "<td id='title-data'><b>Stone Description</b></td</tr>",
+			"data": [
+				"<tr>",
+					"<td>1</td>",
+					"<td>Alexandrite (transparent dark green)</td>",
 				"</tr>",
 				"<tr>",
-					"<td>11</td>",
-					"<td>Tiger eye (translucent brown with golden center)</td>",
+					"<td>2</td>",
+					"<td>Aquamarine (transparent pale blue-green)</td>",
 				"</tr>",
 				"<tr>",
-					"<td>12</td>",
-					"<td>Turquoise (opaque light blue-green)</td>",
-				"</tr>" 
+					"<td>3</td>",
+					"<td>Black pearl (opaque pure black)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>4</td>",
+					"<td>Blue spinel (transparent deep blue)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>5</td>",
+					"<td>Peridot (transparent rich olive green)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>6</td>",
+					"<td>Topaz (transparent golden yellow)</td>",
+				"</tr>",
+			],
+		},
+		"thousandGems": {
+			"name": "<tr><th id='title-header' colspan='2'>1000gp Gemstones</th></tr>",
+			"die": "<tr><td id='title-data'><b>d8</b></td>",
+			"description": "<td id='title-data'><b>Stone Description</b></td</tr>",
+			"data": [
+				"<tr>",
+					"<td>1</td>",
+					"<td>Black opal (translucent dark green with black mottling and golden flecks)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>2</td>",
+					"<td>Blue sapphire (transparent blue-white to medium blue)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>3</td>",
+					"<td>Emerald (transparent deep bright green)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>4</td>",
+					"<td>Fire opal (translucent fiery red)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>5</td>",
+					"<td>Opal (translucent pale blue with green and golden mottling)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>6</td>",
+					"<td>Star ruby (translucent ruby with white star-shaped center)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>7</td>",
+					"<td>Star sapphire (translucent blue sapphire with white star-shaped center)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>8</td>",
+					"<td>Yellow sapphire (transparent fiery yellow or yellow green)</td>",
+				"</tr>",
+			],
+		},
+		"fivethousandGems": {
+			"name": "<tr><th id='title-header' colspan='2'>5000gp Gemstones</th></tr>",
+			"die": "<tr><td id='title-data'><b>d4</b></td>",
+			"description": "<td id='title-data'><b>Stone Description</b></td</tr>",
+			"data": [
+				"<tr>",
+					"<td>1</td>",
+					"<td>Black sapphire (translucent lustrous black with glowing highlights)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>2</td>",
+					"<td>Diamond (transparent blue-white , canary, pink, brown, or blue)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>3</td>",
+					"<td>Jacinth (transparent fiery orange)</td>",
+				"</tr>",
+				"<tr>",
+					"<td>4</td>",
+					"<td>Ruby (transparent clear red to deep crimson)</td>",
+				"</tr>",
 			],
 		},
 		
+	},
+}
+
+var art = {
+	"price": {
+		"twentyfiveArt": {
+			"name": "<tr><th id='title-header' colspan='2'>25gp Art Objects</th></tr>", 
+			"die": "<tr><td id='title-data'><b>d10</b></td>",
+			"description": "<td id='title-data'><b>Stone Description</b></td</tr>",
+			"data": [
+				"<tr>",
+					"<td>1</td>",
+					"<td>Silver ewer</td>",
+				"</tr>",
+				"<tr>",
+					"<td>2</td>",
+					"<td>Carved bone statuette</td>",
+				"</tr>",
+				"<tr>",
+					"<td>3</td>",
+					"<td>Small gold bracelet</td>",
+				"</tr>",
+				"<tr>",
+					"<td>4</td>",
+					"<td>Cloth-of-gold vestments</td>",
+				"</tr>",
+				"<tr>",
+					"<td>5</td>",
+					"<td>Black velvet mask stitched with silver thread</td>",
+				"</tr>",
+				"<tr>",
+					"<td>6</td>",
+					"<td>Copper chalice with silver filigree</td>",
+				"</tr>",
+				"<tr>",
+					"<td>7</td>",
+					"<td>Pair of engraved bone dice</td>",
+				"</tr>",
+				"<tr>",
+					"<td>8</td>",
+					"<td>Small mirror set in a painted wooden frame</td>",
+				"</tr>",
+				"<tr>",
+					"<td>9</td>",
+					"<td>Embroidered silk handkerchief</td>",
+				"</tr>",
+				"<tr>",
+					"<td>10</td>",
+					"<td>Gold locket with a painted portrait inside</td>",
+				"</tr>",
+			],
+		},
+		"twofiftyArt": {
+			"name": "<tr><th id='title-header' colspan='2'>250gp Art Objects</th></tr>", 
+			"die": "<tr><td id='title-data'><b>d10</b></td>",
+			"description": "<td id='title-data'><b>Stone Description</b></td</tr>",
+			"data": [
+				"<tr>",
+					"<td>1</td>",
+					"<td>Gold ring set with bloodstones</td>",
+				"</tr>",
+				"<tr>",
+					"<td>2</td>",
+					"<td>Carved ivory statuette</td>",
+				"</tr>",
+				"<tr>",
+					"<td>3</td>",
+					"<td>Large gold bracelet</td>",
+				"</tr>",
+				"<tr>",
+					"<td>4</td>",
+					"<td>Silver necklace with a gemstone pendant</td>",
+				"</tr>",
+				"<tr>",
+					"<td>5</td>",
+					"<td>Bronze crown</td>",
+				"</tr>",
+				"<tr>",
+					"<td>6</td>",
+					"<td>Silk robe with gold embroidery</td>",
+				"</tr>",
+				"<tr>",
+					"<td>7</td>",
+					"<td>Large well-made tapestry</td>",
+				"</tr>",
+				"<tr>",
+					"<td>8</td>",
+					"<td>Brass mug with jade inlay</td>",
+				"</tr>",
+				"<tr>",
+					"<td>9</td>",
+					"<td>Box of turquoise animal figurines</td>",
+				"</tr>",
+				"<tr>",
+					"<td>10</td>",
+					"<td>Gold bird cage with electrum filigree</td>",
+				"</tr>",
+			],
+		},
+		"sevenfiftyArt": {
+			"name": "<tr><th id='title-header' colspan='2'>750gp Art Objects</th></tr>", 
+			"die": "<tr><td id='title-data'><b>d10</b></td>",
+			"description": "<td id='title-data'><b>Stone Description</b></td</tr>",
+			"data": [
+				"<tr>",
+					"<td>1</td>",
+					"<td>Silver chalice set with moonstones</td>",
+				"</tr>",
+				"<tr>",
+					"<td>2</td>",
+					"<td>Silver-plated steel longsword with jet set in hilt</td>",
+				"</tr>",
+				"<tr>",
+					"<td>3</td>",
+					"<td>Carved harp of exotic wood with ivory inlay and zircon gems</td>",
+				"</tr>",
+				"<tr>",
+					"<td>4</td>",
+					"<td>Small gold idol</td>",
+				"</tr>",
+				"<tr>",
+					"<td>5</td>",
+					"<td>Gold dragon comb set with red garnets as eyes</td>",
+				"</tr>",
+				"<tr>",
+					"<td>6</td>",
+					"<td>Bottle stopper cork embossed with gold leaf and set with amethysts</td>",
+				"</tr>",
+				"<tr>",
+					"<td>7</td>",
+					"<td>Ceremonial electrum dagger wit~ a black pearl in the pommel</td>",
+				"</tr>",
+				"<tr>",
+					"<td>8</td>",
+					"<td>Silver and gold brooch</td>",
+				"</tr>",
+				"<tr>",
+					"<td>9</td>",
+					"<td>Obsidian statuette with gold fittings and inlay</td>",
+				"</tr>",
+				"<tr>",
+					"<td>10</td>",
+					"<td>Painted gold war mask</td>",
+				"</tr>",
+			],
+		},
+		"twentyfivehundredArt": {
+			"name": "<tr><th id='title-header' colspan='2'>2500gp Art Objects</th></tr>", 
+			"die": "<tr><td id='title-data'><b>d10</b></td>",
+			"description": "<td id='title-data'><b>Stone Description</b></td</tr>",
+			"data": [
+				"<tr>",
+					"<td>1</td>",
+					"<td>Fine gold chain set with a fire opal</td>",
+				"</tr>",
+				"<tr>",
+					"<td>2</td>",
+					"<td>Old masterpiece painting</td>",
+				"</tr>",
+				"<tr>",
+					"<td>3</td>",
+					"<td>Embroidered silk and velvet mantle set with numerous moonstones</td>",
+				"</tr>",
+				"<tr>",
+					"<td>4</td>",
+					"<td>Platinum bracelet set with a sapphire</td>",
+				"</tr>",
+				"<tr>",
+					"<td>5</td>",
+					"<td>Embroidered glove set with jewel chips</td>",
+				"</tr>",
+				"<tr>",
+					"<td>6</td>",
+					"<td>Jeweled anklet</td>",
+				"</tr>",
+				"<tr>",
+					"<td>7</td>",
+					"<td>Gold music box</td>",
+				"</tr>",
+				"<tr>",
+					"<td>8</td>",
+					"<td>Gold circlet set with four aquamarines</td>",
+				"</tr>",
+				"<tr>",
+					"<td>9</td>",
+					"<td>Eye patch with a mock eye set in blue sapphire and moonstone</td>",
+				"</tr>",
+				"<tr>",
+					"<td>10</td>",
+					"<td>A necklace string of small pink pearls</td>",
+				"</tr>",
+			],
+		},
+		"seventyfivehundredArt": {
+			"name": "<tr><th id='title-header' colspan='2'>7500gp Art Objects</th></tr>", 
+			"die": "<tr><td id='title-data'><b>d8</b></td>",
+			"description": "<td id='title-data'><b>Stone Description</b></td</tr>",
+			"data": [
+				"<tr>",
+					"<td>1</td>",
+					"<td>Jeweled gold crown</td>",
+				"</tr>",
+				"<tr>",
+					"<td>2</td>",
+					"<td>Jeweled platinum ring</td>",
+				"</tr>",
+				"<tr>",
+					"<td>3</td>",
+					"<td>Small gold statuette set with rubies</td>",
+				"</tr>",
+				"<tr>",
+					"<td>4</td>",
+					"<td>Gold cup set with emeralds</td>",
+				"</tr>",
+				"<tr>",
+					"<td>5</td>",
+					"<td>Gold jewelry box with platinum filigree</td>",
+				"</tr>",
+				"<tr>",
+					"<td>6</td>",
+					"<td>Painted gold child's sarcophagus</td>",
+				"</tr>",
+				"<tr>",
+					"<td>7</td>",
+					"<td>Jade game board with solid gold playing pieces</td>",
+				"</tr>",
+				"<tr>",
+					"<td>8</td>",
+					"<td>Bejeweled ivory drinking horn with gold filigree</td>",
+				"</tr>",
+			],
+		},
 	},
 }
 
@@ -173,6 +490,8 @@ var gems = {
 //**********************************
 //* beginning of display scripting *
 //**********************************
+
+//choose table based on name
 function tableChoice(tname){
 	var name = tname.name;
 	var die = tname.die;
@@ -182,28 +501,56 @@ function tableChoice(tname){
 	return str;
 }
 
+//gems
 var tengems = tableChoice(gems.price.tenGems);
 var fiftygems = tableChoice(gems.price.fiftyGems);
+var hundredgems = tableChoice(gems.price.hundredGems);
+var fivehundredgems = tableChoice(gems.price.fivehundredGems);
+var thousandgems = tableChoice(gems.price.thousandGems);
+var fivethousandgems = tableChoice(gems.price.fivethousandGems);
+
+//art
+var twentyfiveart = tableChoice(art.price.twentyfiveArt);
+var twofiftyart = tableChoice(art.price.twofiftyArt);
+var sevenfiftyart = tableChoice(art.price.sevenfiftyArt);
+var twentyfivehundredart = tableChoice(art.price.twentyfivehundredArt);
+var seventyfivehundredart = tableChoice(art.price.seventyfivehundredArt);
+
 
 //logic for dropdown filter selection
 function changeTable(){
-	if($("select#tablefilter").val() == 'All'){
-		$("h4#main-header").html("All");
-		$("table.loot-table").html("");
-		$("table#mytable1").html(tengems);
-	}
 	if($("select#tablefilter").val() == 'Gemstones'){
+		$("table.loot-table").html("");
 		$("h4#main-header").html("Gemstones");
 		$("table#mytable1").html(tengems);
 		$("table#mytable2").html(fiftygems);
-	}
+		$("table#mytable3").html(hundredgems);
+		$("table#mytable4").html(fivehundredgems);
+		$("table#mytable5").html(thousandgems);
+		$("table#mytable6").html(fivethousandgems);
+		
+	};
+	if($("select#tablefilter").val() == 'Art'){
+		$("table.loot-table").html("");
+		$("h4#main-header").html("Art Objects");
+		$("table#mytable1").html(twentyfiveart);
+		$("table#mytable2").html(twofiftyart);
+		$("table#mytable3").html(sevenfiftyart);
+		$("table#mytable4").html(twentyfivehundredart);
+		$("table#mytable5").html(seventyfivehundredart);
+		
+	};
 }
 
 //onload of page
 function loadTable(){
-	$("h4#main-header").html("All");
+	$("h4#main-header").html("Gemstones");
 	$("table#mytable1").html(tengems);
-	//$("table#mytable2").html(fiftygems);
+	$("table#mytable2").html(fiftygems);
+	$("table#mytable3").html(hundredgems);
+	$("table#mytable4").html(fivehundredgems);
+	$("table#mytable5").html(thousandgems);
+	$("table#mytable6").html(fivethousandgems);
 }
 
 //main page initialization
