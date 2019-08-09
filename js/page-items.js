8338adf798f6af7de2a3db35f2b3759d35dcda0e
r3zn3t <34678544+r3zn3t@users.noreply.github.com>
2019-08-09 01:58:46 -0600

    Updated Superheavy STR requirement
    Updated the STR requirement for Exotic Weapon Property "Superheavy" to 18

--- js/page-items.js
+++ js/page-items.js
@@ -403,7 +403,7 @@ function useitem (id) {
 			if (b === "SWI") a = a + " ("+ curitem.dmg2 + ")";
 			if (b === "T" || b === "A") a = a + " (" + curitem.range + "ft.)";
 			if (b === "RLD") a = a + " (" + curitem.reload + " shots)";
-			if (b === "SUP") a = a + " (STR 17) ";
+			if (b === "SUP") a = a + " (STR 18) ";
 			if (i > 0) a = ", "+a;
 			$("span#properties").append(a);
 		}
